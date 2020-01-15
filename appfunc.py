d75e5244b14870cc9a08f6a9ceeaeedd2cb1e06c
shakilattar <59828272+shakilattar@users.noreply.github.com>
2020-01-15 18:46:08 +0530

    Add files via upload

---
+++ appfunc.py
@@ -0,0 +1,11 @@
+# append method
+def func():
+    numbers = [121, 144, 169, 196, 225]
+    print(numbers)
+    # append a new value
+    # add 256 to the end of the list
+    numbers.append(256)
+    print(numbers)
+
+
+func()
